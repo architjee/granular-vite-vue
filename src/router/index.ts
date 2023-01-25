29fb48584c394d8c94558f2d254c5677e6f51336
Archit Jain <32292295+architjee@users.noreply.github.com>
2023-01-25 12:45:36 +0530

    index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,12 +10,20 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/:location',
+      name: 'location',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: HomeView
+    },
+    {
+      path: '/:location/:placeid',
+      name: 'placeid',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: HomeView
     }
   ]
 })
