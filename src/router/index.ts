ae97f51500bec1fcb8060743ed606ff8c0afdd36
Archit Jain <32292295+architjee@users.noreply.github.com>
2023-01-25 12:51:50 +0530

    Removing dummy elements

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,17 +12,11 @@ const router = createRouter({
     {
       path: '/:location',
       name: 'location',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: HomeView
     },
     {
       path: '/:location/:placeid',
       name: 'placeid',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: HomeView
     }
   ]
