c0d2d2021898868d34f5ce649d19148a65964951
Archit Jain <32292295+architjee@users.noreply.github.com>
2023-01-25 14:02:56 +0530

    Working map at root

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div class="field has-addons">
+        <div class="control">
+            <input class="input" type="text" placeholder="Find a repository">
+        </div>
+        <div class="control">
+            <a class="button is-info">
+                Search
+            </a>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+  methods: {
+    submit() {
+      this.$emit('someEvent')
+    }
+  }
+}
+</script>
