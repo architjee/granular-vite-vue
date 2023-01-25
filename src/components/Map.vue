f6489b738ca15f5d1659d76991f2a4351e610f2e
Archit Jain <32292295+architjee@users.noreply.github.com>
2023-01-25 17:54:23 +0530

    map component created

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,17 +1,15 @@
 <script lang="ts">
 import L from 'leaflet/dist/leaflet.js'
-console.log('checking the window object for L', L)
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   // type inference enabled
   props: {
-    msg: { type: String, required: true }
+   
   },
   data() {
     return {
       map: {},
-      count: 1
     }
   },
   mounted() {
@@ -22,29 +20,52 @@ export default defineComponent({
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
     console.log('this.map', this.map)
-    
 
-	const circle = L.circle([51.508, -0.11], {
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5,
-		radius: 500
-	}).addTo(this.map);
 
-	const polygon = L.polygon([
-		[51.509, -0.08],
-		[51.503, -0.06],
-		[51.51, -0.047]
-	]).addTo(this.map);
+    const circle = L.circle([51.508, -0.11], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+    }).addTo(this.map);
+
+    const polygon = L.polygon([
+      [51.509, -0.08],
+      [51.503, -0.06],
+      [51.51, -0.047]
+    ]).addTo(this.map);
 
   }
 })
 </script>
 
-<template>
- 
+<template> 
+ <div class="card">
+      <div class="card-image">
+        <figure class="image">
+          <div id="map" class="image is-2by1"></div>
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
             <figure class="image is-48x48">
               <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
             </figure>
-       
+          </div>
+          <div class="media-content">
+            <p class="title is-4">John Smith</p>
+            <p class="subtitle is-6">@johnsmith</p>
+          </div>
+        </div>
+
+        <div class="content">
+          Population of this location is :
+          <a href="#">#css</a> <a href="#">#responsive</a>
+          <br>
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+      </div>
+    </div>
+
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,42 +1,23 @@
 <script lang="ts">
-import L from 'leaflet/dist/leaflet.js'
-console.log('checking the window object for L', L)
+import MapVue from '../components/Map.vue'
+console.log('checking the window object for L')
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   // type inference enabled
+  components:{
+    MapVue
+  },
   props: {
-    msg: { type: String, required: true }
+   
   },
   data() {
     return {
-      map: {},
       count: 1
     }
   },
   mounted() {
-    console.log('this.map', this.map)
-    this.map = L.map('map').setView([51.505, -0.09], 13)
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this.map);
-    console.log('this.map', this.map)
-    
-
-	const circle = L.circle([51.508, -0.11], {
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5,
-		radius: 500
-	}).addTo(this.map);
-
-	const polygon = L.polygon([
-		[51.509, -0.08],
-		[51.503, -0.06],
-		[51.51, -0.047]
-	]).addTo(this.map);
-
+   
   }
 })
 </script>
@@ -50,32 +31,8 @@ export default defineComponent({
         <!-- <strong>sections</strong>  -->
       </h2>
     </section>
-    <div class="card">
-      <div class="card-image">
-        <figure class="image">
-          <div id="map" class="image is-2by1"></div>
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="media">
-          <div class="media-left">
-            <figure class="image is-48x48">
-              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="media-content">
-            <p class="title is-4">John Smith</p>
-            <p class="subtitle is-6">@johnsmith</p>
-          </div>
-        </div>
 
-        <div class="content">
-          Population of this location is :
-          <a href="#">#css</a> <a href="#">#responsive</a>
-          <br>
-          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-        </div>
-      </div>
-    </div>
+    <MapVue></MapVue>
+   
   </main>
 </template>
